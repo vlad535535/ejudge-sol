5aacdc3735fbe9bc2a893ca0fcc80170cd00a56c
mb1te <mb1te.comics@gmail.com>
2018-12-19 16:47:50 +0300

    1 семестр, разные, Р

--- Программирование - 1 семестр/12 - Разные задачи/P.cpp
+++ Программирование - 1 семестр/12 - Разные задачи/P.cpp
@@ -4,24 +4,32 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
-#include <iomanip>
 #include <string>
-#include <fstream>
+#include <iomanip>
+
 
 using namespace std;
 
-int main() {
-	int k, n, t, cnt;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		cin >> k >> n;
-		int tmp = k; cnt = 1;
-		if (k > n) cout << 1 << " " << n << endl;
-		else {
-			while (tmp + k < n) { tmp += k; cnt++; }
-			cout << cnt + 1 << " " << n % tmp << endl;
-		}
+int t, n, sum, s[100], p[500000];
+
+string solution() {
+	for (int i = 0; i < 500000; i++) p[i] = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> s[i];
+	if (n == 1) return "Yes";
+	for (int i = 1; i < n; i++) if (s[i - 1] >= s[i]) return "No";
+	p[s[0] + s[1]] = 1; sum = s[n - 1] * n;
+	for (int i = 2; i < n; i++) {
+		if(p[s[i]]) return "No";
+		for (int j = 0; j < i; j++) p[s[i] + s[j]] = 1;
+		for (int j = sum; j >= s[i]; j--) if (p[j - s[i]]) p[j] = 1;
 	}
+	return "Yes";
+}
+
+int main() { 
+	cin >> t;
+	while (t--) cout << solution() << endl;
 	//system("pause");
 	return 0;
 }
